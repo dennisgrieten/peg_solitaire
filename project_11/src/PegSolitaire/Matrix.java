ba9e6cb5df7ade7e2e1d6b9a8f27571cc9ef53fb
Dennis Grieten <dennisgrieten@gmail.com>
2015-01-28 15:24:42 +0100

    Start

--- project_11/src/PegSolitaire/Matrix.java
+++ project_11/src/PegSolitaire/Matrix.java
@@ -1,8 +1,11 @@
 package PegSolitaire;
 
+import java.util.Stack;
+
 /**
  * Created by dennis on 28/01/15.
  */
 public class Matrix {
     Vak[][] matrix = new Vak[7][7];
+    Stack<Ball> stack;  // Stack voor verwijderde ballen
 }
