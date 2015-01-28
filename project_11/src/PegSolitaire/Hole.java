d58392af837887c06b5cb99c9df90166af8e6738
Dennis Grieten <dennisgrieten@gmail.com>
2015-01-28 16:48:15 +0100

    Start

--- project_11/src/PegSolitaire/Hole.java
+++ project_11/src/PegSolitaire/Hole.java
@@ -4,26 +4,26 @@ package PegSolitaire;
  * Created by dennis on 28/01/15.
  */
 public class Hole {
-    private Ball bal;
+    private Ball ball;
     boolean deadZone = false; // Vlag voor dode hoeken
 
-    public Hole(Ball bal) {
-        this.bal = bal;
+    public Hole(Ball ball) {
+        this.ball = ball;
     }
 
-    public Hole(Ball bal, boolean deadZone) {
-        this.bal = bal;
+    public Hole(Ball ball, boolean deadZone) {
+        this.ball = ball;
         this.deadZone = deadZone;
     }
 
     public void setBall(Ball bal) {
-        this.bal = bal;
+        this.ball = bal;
     }
 
-    // Overhandig de bal
+    // Overhandig de ball
     public Ball giveBall() {
-        Ball out = this.bal;
-        this.bal = null;    // Wis pointer van bal in dit vak
+        Ball out = this.ball;
+        this.ball = null;    // Wis pointer van bal in dit vak
         return out;
     }
 }
