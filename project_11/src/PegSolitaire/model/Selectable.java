2ab1645dc4a7c59227d3fb03f4a39e9d2a841c5a
Dennis Grieten <dennisgrieten@gmail.com>
2015-02-05 21:56:36 +0100

    Selectable met methode selectable + overloading

--- project_11/src/PegSolitaire/model/Selectable.java
+++ project_11/src/PegSolitaire/model/Selectable.java
@@ -4,4 +4,8 @@ package PegSolitaire.model;
  * Created by dennis on 5/02/15.
  */
 public interface Selectable {
+
+    public boolean selectable();
+
+    public boolean selectable(boolean emptyField);
 }
