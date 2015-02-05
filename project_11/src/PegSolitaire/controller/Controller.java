9e81653e6cbdbbe453a522c578e53813619d0e80
Dennis Grieten <dennisgrieten@gmail.com>
2015-02-05 10:17:48 +0100

    Undo methoden gecomment

--- project_11/src/PegSolitaire/controller/Controller.java
+++ project_11/src/PegSolitaire/controller/Controller.java
@@ -1,12 +1,13 @@
 package PegSolitaire.controller;
 
 import PegSolitaire.model.Matrix;
+import java.lang.String;
 
 /**
  * Created by dennis on 28/01/15.
  */
 public class Controller {
-    private Matrix matrix;
+    private Matrix matrix = new Matrix();
 
     /* Doe een zet */
     public void doMove(int x0, int y0, int x1, int y1) {
