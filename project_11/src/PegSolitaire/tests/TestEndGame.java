714e17f0271b0a113d7e390fa05a08281568a702
Dennis Grieten <dennisgrieten@gmail.com>
2015-02-19 08:51:59 +0100

    TestEndGame aangepast

--- project_11/src/PegSolitaire/tests/TestEndGame.java
+++ project_11/src/PegSolitaire/tests/TestEndGame.java
@@ -4,7 +4,6 @@ import PegSolitaire.controller.Game;
 import PegSolitaire.exceptions.IllegalCoordinateException;
 import PegSolitaire.exceptions.IllegalMoveException;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -13,6 +12,39 @@ import java.util.Scanner;
 public class TestEndGame {
     public static void main(String[] args) {
         Game game = new Game();
+        int[] c = {
+                1, 3, 3, 3,
+                2, 5, 2, 3,
+                0, 4, 2, 4,
+                3, 4, 1, 4,
+                5, 4, 3, 4,
+                4, 6, 4, 4,
+                4, 3, 4, 5,
+                2, 6, 4, 6,
+                4, 6, 4, 4,
+                2, 2, 2, 4,
+                2, 0, 2, 2,
+                4, 1, 4, 3,
+                4, 3, 4, 5,
+                4, 5, 2, 5,
+                2, 5, 2, 3,
+                2, 3, 2, 1,
+                0, 2, 0, 4,
+                0, 4, 2, 4,
+                2, 4, 4, 4,
+                6, 2, 4, 2,
+                3, 2, 5, 2,
+                6, 4, 6, 2,
+                6, 2, 4, 2,
+                4, 0, 2, 0,
+                2, 0, 2, 2,
+                1, 2, 3, 2,
+                3, 2, 5, 2,
+                5, 2, 5, 4,
+                5, 4, 3, 4,
+                3, 4, 3, 2,
+                3, 1, 3, 3,
+        };
         Scanner kb = new Scanner(System.in);
         int x = 0, y = 0, x1 = 0, y1 = 0;
         boolean exit = false;
@@ -22,48 +54,19 @@ public class TestEndGame {
             System.out.println(game.printField());
             System.out.print("Plot moves\n\n");
 
-            try {
-                game.doMove(1, 3, 3, 3);
-                game.doMove(2, 5, 2, 3);
-                game.doMove(0, 4, 2, 4);
-                game.doMove(3, 4, 1, 4);
-                game.doMove(5, 4, 3, 4);
-                game.doMove(4, 6, 4, 4);
-                game.doMove(4, 3, 4, 5);
-                game.doMove(2, 6, 4, 6);
-                game.doMove(4, 6, 4, 4);
-                game.doMove(2, 2, 2, 4);
-                game.doMove(2, 0, 2, 2);
-                game.doMove(4, 1, 4, 3);
-                game.doMove(4, 3, 4, 5);
-                game.doMove(4, 5, 2, 5);
-                game.doMove(2, 5, 2, 3);
-                game.doMove(2, 3, 2, 1);
-                game.doMove(0, 2, 0, 4);
-                game.doMove(0, 4, 2, 4);
-                game.doMove(2, 4, 4, 4);
-                game.doMove(6, 2, 4, 2);
-                game.doMove(3, 2, 5, 2);
-                game.doMove(6, 4, 6, 2);
-                game.doMove(6, 2, 4, 2);
-                game.doMove(4, 0, 2, 0);
-                game.doMove(2, 0, 2, 2);
-                game.doMove(1, 2, 3, 2);
-                game.doMove(3, 2, 5, 2);
-                game.doMove(5, 2, 5, 4);
-                game.doMove(5, 4, 3, 4);
-                game.doMove(3, 4, 3, 2);
-                game.doMove(3, 1, 3, 3);
+            for (int i = 0; i < c.length;) {
+                try {
+                    game.doMove(c[i++],c[i++],c[i++],c[i++]);
+                    System.out.println(game.printField());
 
-                System.out.println(game.printField());
+                    if (game.isEndgame()) {
+                        System.out.println("You've unlocked the secret to the universe!\nWell done!");
+                        exit = true;
+                    }
 
-                if (game.isEndgame()) {
-                    System.out.println("You've unlocked the secret to the universe!\nWell done!");
-                    exit = true;
+                } catch (IllegalMoveException | IllegalCoordinateException e) {
+                    System.out.println(e.getMessage() + "\n");
                 }
-
-            } catch (IllegalMoveException | IllegalCoordinateException e) {
-                System.out.println(e.getMessage() + "\n");
             }
 
         } while (!exit);
